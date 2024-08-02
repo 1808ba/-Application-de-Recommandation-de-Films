999f57530783a0980e8dc49d1df5566cd6d73fea
1808ba <bendoumaaziza8@gmail.com>
2024-08-02 19:21:20 +0100

    befErr

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref, onMounted} from 'vue'
+import  axios  from 'axios'
+
+const user =ref();
+onMounted(async()=>{
+    const data =await axios.get('/api/user');
+    console.log(data);
+})
+</script>
+<template>
+</template>
---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,44 @@
+
+<script setup>
+</script>
+<template>
+<h1>register</h1>
+<div class="bg-gray-100 flex items-center justify-center min-h-screen">
+
+<div class="w-full max-w-md bg-white rounded-lg shadow-md p-8">
+  <h2 class="text-2xl font-bold text-gray-800 text-center mb-6">Register</h2>
+  
+  <form>
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Name">
+    </div>
+
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email">
+    </div>
+    
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********">
+    </div>
+    
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="confirm-password">Confirm Password</label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirm-password" type="password" placeholder="********">
+    </div>
+    
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Register
+      </button>
+      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+        Already have an account?
+      </a>
+    </div>
+  </form>
+</div>
+
+</div>
+</template>
